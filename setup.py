67ea9b813c099169fc40260e6f49e838e8fa86f4
Simon Müller <simon.mueller@muon-stat.com>
2018-04-20 00:21:37 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+setup(name = 'oddstream', version = '0.1', description = 'Fill this', url = 'http://github.com/anofox/oddstream', author = 'Simon Müller', author_email = 'sm@anofox.com', licence = 'MIT', packages = ['oddstream'], zip_safe = False)
+ 
