9881534711df54c7353b6e953115ed5e2aa9b625
Simon Müller <simon.mueller@muon-stat.com>
2018-04-23 01:05:24 +0200

    code shipping

--- oddstream/set_outlier_threshold.py
+++ oddstream/set_outlier_threshold.py
@@ -12,16 +12,15 @@ def set_outlier_threshold(pc_norm, p_rate, trials):
     f_extreme = [0 for _ in range(0, m)]
     for t in range(0, trials):
         sample = np.random.choice(range(0, m), m, replace=True)
-        sigma = 0 # TODO
+        # TODO: not nice, but should work
+        sigma = np.cov(pc_norm)
         f_extreme[t] = np.min([fhat.evaluate_points(mvrnorm(n=1, mu=pc_norm[s, :], Sigma=sigma)) for s in sample])
 
     k = 1 / (2 * math.pi)
-    psi_trans = [0 for _ in range(0, m)]
-    for i in range(0, trials):
-        if f_extreme[i] < k:
-            psi_trans[i] = np.sqrt(-2 * np.log(f_extreme[i]) - 2 * np.log(2 * math.pi))
+    # TODO: why np.sqrt(-2 * np.log(f_val) - 2 * np.log(2 * math.pi))?
+    psi_trans = [np.sqrt(-2 * np.log(f_val) - 2 * np.log(2 * math.pi)) if f_val < k else 0.0 for f_val in f_extreme]
 
-    p = np.count_nonzero(f_extreme) / trials
+    p = np.count_nonzero(psi_trans) / trials
     y = - np.log(-1 * np.log(1 - p_rate * p))
     cm = np.sqrt(2 * np.log(m)) - ((np.log(np.log(m)) + np.log(4 * math.pi)) / (2 * np.sqrt(2 * np.log(m))))
     dm = 1 / (np.sqrt(2 * np.log(m)))
