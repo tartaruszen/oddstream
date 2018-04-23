fix threshold calculation

build/lib/oddstream/kde_estimation.py
@@ -1,12 +1,51 @@
 from fastkde import fastKDE
+import numpy as np
 
 """
-    Fast 2D Kernel Density Estimation with point evaluation
+    Fast 2D Kernel Density Estimation with simple point evaluation
 """
 class KDEEstimation2D(object):
     def __init__(self, X):
-        self.pdfObject = fastKDE.fastKDE(X, doSaveMarginals=False, doFFT = False, positiveShift = False, logAxes = False)
-        self.pdfObject.applyBernacchiaFilter()
+        self.pdf, self.axes = fastKDE.pdf(X[:, 0], X[:, 1])
 
     def evaluate_points(self, X):
-        return self.pdfObject.__transformphiSC_points__(X)
+        m = X.shape[0]
+        values = np.array(range(0, m), dtype=float)
+        for i in range(0, m):
+            values[i] = self.evaluate_pdf_value(X[i, :])
+        return values
+
+    def evaluate_pdf_value(self, s):
+        x_up = s[0] <= self.axes[0]
+        index_up_x = self.get_index_upper(x_up, 0)
+        x_low = s[0] >= self.axes[0]
+        index_low_x = self.get_index_lower(x_low)
+
+        y_up = s[1] <= self.axes[1]
+        index_up_y = self.get_index_upper(y_up, 1)
+        y_low = s[1] >= self.axes[1]
+        index_low_y = self.get_index_lower(y_low)
+
+        # TODO
+        value = 0.0
+        for i in range(index_low_x, index_up_x + 1):
+            for j in range(index_low_y, index_up_y + 1):
+                value += self.pdf.T[i, j]
+        value /= 4
+        return value
+
+    def get_index_upper(self, values, index):
+        c = [i for i in range(0, len(values)) if values[i]]
+        if len(c) == 0:
+            up = self.pdf.shape[index] - 2
+        else:
+            up = np.min(c)
+        return up
+
+    def get_index_lower(self, values):
+        c = [i for i in range(0, len(values)) if values[i]]
+        if len(c) == 0:
+            up = 0
+        else:
+            up = np.max(c)
+        return up

build/lib/oddstream/set_outlier_threshold.py
@@ -8,17 +8,21 @@ from oddstream.utils import mvrnorm
 def set_outlier_threshold(pc_norm, p_rate, trials):
     fhat = KDEEstimation2D(pc_norm)
     m = pc_norm.shape[0]
+    d = pc_norm.shape[1]
 
-    f_extreme = [0 for _ in range(0, m)]
+    # TODO: not nice, but should work
+    Sigma = np.cov(pc_norm.T)
+    f_extreme = np.array([0 for _ in range(0, m)], dtype = float)
+    r_sample = np.array([[0.0 for i in range(d)] for j in range(m)], dtype=float)
     for t in range(0, trials):
         sample = np.random.choice(range(0, m), m, replace=True)
-        # TODO: not nice, but should work
-        Sigma = np.cov(pc_norm.T)
-        f_extreme[t] = np.min([fhat.evaluate_points(mvrnorm(n=1, mu=pc_norm[s, :], Sigma=Sigma)) for s in sample])
+        for s in range(0, m):
+            r_sample[s, :] = mvrnorm(n=1, mu=pc_norm[sample[s], :], Sigma=Sigma)
+        f_extreme[t] = np.min(fhat.evaluate_points(r_sample))
 
     k = 1 / (2 * math.pi)
     # TODO: why np.sqrt(-2 * np.log(f_val) - 2 * np.log(2 * math.pi))?
-    psi_trans = [np.sqrt(-2 * np.log(f_val) - 2 * np.log(2 * math.pi)) if f_val < k else 0.0 for f_val in f_extreme]
+    psi_trans = [1.0 if f_val < k else 0.0 for f_val in f_extreme]
 
     p = np.count_nonzero(psi_trans) / trials
     y = - np.log(-1 * np.log(1 - p_rate * p))
@@ -26,5 +30,5 @@ def set_outlier_threshold(pc_norm, p_rate, trials):
     dm = 1 / (np.sqrt(2 * np.log(m)))
     t = cm + y * dm
     threshold_fnx = np.exp(-1 * ((t**2) + 2 * np.log(2 * math.pi)) / 2)
-    return {'threshold_fnx': threshold_fnx, 'fhat': fhat, 'Sigma': sigma}
+    return {'threshold_fnx': threshold_fnx, 'fhat': fhat, 'Sigma': Sigma}
 

oddstream/test.py
@@ -1,10 +1,11 @@
 import oddstream
 from sklearn.decomposition import PCA
 import numpy as np
-from oddstream import set_outlier_threshold
+from oddstream.set_outlier_threshold import set_outlier_threshold
 from oddstream.kde_estimation import KDEEstimation2D
 from oddstream.utils import mvrnorm
 from fastkde import fastKDE
+import fastkde
 
 N = int(2e3)
 var1 = 50*np.random.normal(size=N) + 0.1
@@ -14,11 +15,67 @@ v = np.array([var1, var2]).T
 pca = PCA(n_components=2)
 pca.fit(v)
 pc_norm = pca.transform(v)
+kde = KDEEstimation2D(pc_norm)
+kde.evaluate_points(pc_norm)
+
+import pylab as PP
+PP.contor(kde.axes[0], kde.axes[1], kde.pdf)
+PP.show()
+
+PP.pcolor(kde.pdf)
+PP.colorbar()
+PP.show()
+
+PP.pcolor(pc_norm)
+PP.colorbar()
+PP.show()
+
 set_outlier_threshold(pc_norm, 0.5, 100)
 
-fastKDE.pdf_at_points(pc_norm[1:10, 0], pc_norm[1:10, 1])
-kde = KDEEstimation2D(pc_norm.T)
-kde.evaluate_points(pc_norm.T)
+
+
+
+# fastKDE.pdf_at_points(pc_norm[1:10, 0], pc_norm[1:10, 1])
+kde = KDEEstimation2D(pc_norm)
+
+s = pc_norm[0, :]
+x_low = s[0] <= kde.axes[0]
+index_up_x = kde.get_index_upper(x_low, 0)
+x_up = s[0] >= kde.axes[0]
+index_low_x = kde.get_index_lower(x_up)
+
+x_low = s[1] <= kde.axes[1]
+index_up_y = kde.get_index_upper(x_low, 1)
+x_up = s[1] >= kde.axes[1]
+index_low_y = kde.get_index_lower(x_up)
+
+value = 0.0
+for i in range(index_low_x, index_up_x + 1):
+    for j in range(index_low_y, index_up_y + 1):
+        value += kde.pdf.T[i, j]
+value /= 4
+
+
+kde.evaluate_points(pc_norm)
 
 Sigma = np.cov(pc_norm)
 mvrnorm(1, pc_norm[0, :],)
+
+
+def evaluate_pdf_value(s):
+    c1 = s[0] <= axes[0]
+    c2 = s[0] >= axes[0]
+    up_x = np.min([index for index in range(0, len(c1)) if c1[index]])
+    low_x = np.max([index for index in range(0, len(c2)) if c2[index]])
+
+    c1 = s[1] <= axes[1]
+    c2 = s[1] >= axes[1]
+    up_y = np.min([index for index in range(0, len(c1)) if c1[index]])
+    low_y = np.max([index for index in range(0, len(c2)) if c2[index]])
+
+    value = 0.0
+    for i in range(low_x, up_x + 1):
+        for j in range(low_y, up_y + 1):
+            value += pdf[i, j]
+    value /= 4
+    return value